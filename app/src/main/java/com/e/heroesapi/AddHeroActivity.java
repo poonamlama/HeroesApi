6c93b994f14b23f7a76f5bb5adce9cbb69a4ad2c
Softwarica College Lab <softwarica@softwarica.com>
2019-05-22 10:03:57 +0545

    image api

--- app/src/main/java/com/e/heroesapi/AddHeroActivity.java
+++ app/src/main/java/com/e/heroesapi/AddHeroActivity.java
@@ -1,12 +1,16 @@
 package com.e.heroesapi;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 import heroesapi.HeroesAPI;
@@ -22,6 +26,7 @@ public class AddHeroActivity extends AppCompatActivity {
 
     private EditText etName, etDesc;
     private Button btnSave;
+    private ImageView imgPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,8 @@ public class AddHeroActivity extends AppCompatActivity {
         etName=findViewById(R.id.etName);
         etDesc=findViewById(R.id.etDesc);
         btnSave=findViewById(R.id.btnSave);
+        imgPhoto=findViewById(R.id.impPhoto);
+        loadFormUrl();
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,4 +77,22 @@ public class AddHeroActivity extends AppCompatActivity {
             }
 
         });
-}}
+}
+
+    private void StrictMode(){
+        android.os.StrictMode.ThreadPolicy policy = new android.os.StrictMode.ThreadPolicy.Builder().permitAll().build();
+        android.os.StrictMode.setThreadPolicy(policy);
+    }
+    private void loadFormUrl() {
+        StrictMode();
+        try{
+            String imgUrl ="https://cdn.shopify.com/s/files/1/1803/0853/products/Gypsophila_Xlence_1400x.jpg?v=1533680754";
+            URL url= new URL(imgUrl);
+            imgPhoto.setImageBitmap(BitmapFactory.decodeStream((InputStream)url.getContent()));
+        }
+        catch (IOException e) {
+            Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
